3e889fb0782cce6a145f3812c133d390ca637cd6
ryym <ryym.64@gmail.com>
2021-08-09 08:28:27 +0900

    refactor: Extract async function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use crate::{exec::block_on, reactor::Reactor, task::Task};
 use std::time::Instant;
 
 fn main() {
+    block_on(run_futures());
+}
+
+async fn run_futures() {
     let start = Instant::now();
 
     let reactor = Reactor::new();
@@ -22,10 +26,6 @@ fn main() {
         println!("Got {} at time: {:.2}", val, start.elapsed().as_secs_f32());
     };
 
-    let mainfut = async {
-        fut1.await;
-        fut2.await;
-    };
-
-    block_on(mainfut);
+    fut1.await;
+    fut2.await;
 }
